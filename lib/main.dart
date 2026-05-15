0e33d643180dea9d6dc9939af1042ed0b3811010
sidratulpunno <sidratulmuntahapunno845000.com>
2026-05-15 21:40:33 +0600

    created the main structure

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/mood_tracker_screen.dart';
+
+void main() {
+  runApp(const MoodTrackerApp());
+}
+
+class MoodTrackerApp extends StatelessWidget {
+  const MoodTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mood Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6366F1),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: ,
+    );
+  }
+}
